91e4162787911627ba5c2b0e5178f929747e296e
Ayush Singh <82327294+ayush-321@users.noreply.github.com>
2021-08-16 11:36:22 +0530

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,38 +1,36 @@
 import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
+import firebase from 'firebase';
 
 class App extends React.Component {
 
   constructor(){
     super();
     this.state = {
-        products: [
-            {
-                price: 150000,
-                title: 'Mobile',
-                qty: 0,
-                img: 'https://images.unsplash.com/photo-1580910051074-3eb694886505?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bW9iaWxlJTIwcGhvbmV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-                id: 1
-            },
-
-            {
-                price: 500000,
-                title: 'Watch',
-                qty: 0,
-                img: 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2F0Y2h8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-                id: 2
-            },
-
-            {
-                price: 200000,
-                title: 'Laptop',
-                qty: 0,
-                img: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-                id: 3
-            }
-        ]
+        products: [],
+        loading: true
     }
+}
+
+componentDidMount() {
+  firebase
+    .firestore()
+    .collection('products')
+    .get()
+    .then((snapshot) => {
+      const products = snapshot.docs.map((doc) => {
+        const data = doc.data();
+        data['id'] = doc.id;
+        return data;
+      })
+
+      this.setState({
+        products,
+        loading: false
+      })
+
+    })
 }
 
 handleIncreaseQuantity = (product) => {
@@ -84,14 +82,17 @@ getCartTotal = () => {
   let cartTotal = 0;
 
   products.map((product) => {
+    if(product.qty > 0){
     cartTotal += product.qty * product.price;
+    }
+    return '';
   });
 
   return cartTotal;
 }
 
   render() {
-    const { products } = this.state;
+    const { products, loading } = this.state;
   return (
     <div className="App">
       <Navbar count={this.getCount()} />
@@ -101,7 +102,8 @@ getCartTotal = () => {
         onDecreaseQuantity={this.handleDecreaseQuantity}
         onDeleteProduct={this.handleDeleteProduct}
       />
-      <div style={{padding:10, fontSize: 20, textAlign: 'center'}}>TOTAL: { this.getCartTotal() } </div>
+      {loading && <div><h1>Loading Products...</h1></div>}
+      <div style={{padding:10, fontSize: 20}}>TOTAL: { this.getCartTotal() } </div>
     </div>
   );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase';
+import 'firebase/firestore';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB-aW5mPARAibVw4vRMEIdJT-2n60f-N78",
+  authDomain: "cart-68b7e.firebaseapp.com",
+  projectId: "cart-68b7e",
+  storageBucket: "cart-68b7e.appspot.com",
+  messagingSenderId: "308260666656",
+  appId: "1:308260666656:web:5e8cb1ccf4c3fdd8fc6b45"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
